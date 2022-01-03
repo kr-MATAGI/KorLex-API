ace8bda24c20433d189a6aac4ac310a11c2d91ba
Matagi <dev_matagi@kakao.com>
2022-01-03 15:09:47 +0900

    add, db connect py and .gitignore

---
+++ Samples/DB_Connect.py
@@ -0,0 +1,22 @@
+import pyodbc
+import pandas as pd
+
+# set up some constants
+MDB_PATH = 'C:/Users/JaeHoon/Desktop/KoLex/db/20170726_KorLexDB.mdb'
+
+# connect to db
+#cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};DBQ='+MDB+';')
+cnxn = pyodbc.connect('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+MDB_PATH+';')
+cursor = cnxn.cursor()
+
+queryArr = []
+queryArr.append("SELECT")
+queryArr.append("*")
+queryArr.append("FROM tblWN_SSINfo")
+queryStr = " ".join(queryArr)
+
+rows = pd.read_sql(queryStr, cnxn)
+
+print(len(rows))
+print(rows["fldXml"])
+cnxn.close()
