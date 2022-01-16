3566b2d8c6c3815a31d789ab834015f3316446bb
kr-MATAGI <dev_matagi@kakao.com>
2022-01-16 15:29:42 +0900

    add, w2ss_dic_path ss2w_dic_path

--- KorLex-api/krx_json_api.py
+++ KorLex-api/krx_json_api.py
@@ -12,9 +12,34 @@ from korLexDef import *
 class KorLexAPI:
     ### PRIVATE ###
     ### METHOD ###
-    def __init__(self, json_path:str):
-        self.is_set_json_path = False
+    def __init__(self, json_path:str, w2ss_path:str, ss2w_path:str):
+        print("[KorLexAPI][INIT] Plz Wait...")
 
+        self.is_set_json_path = False
+        self.is_set_w2ss_path = False
+        self.is_set_ss2w_path = False
+
+        # check w2ss_path, ss2w_path
+        if 0 >= len(w2ss_path):
+            print("[KorLexAPI][INIT] ERR - Plz check w2ss_path:", w2ss_path)
+            return
+        if not os.path.exists(w2ss_path):
+            print("[KorLexAPI][INIT] ERR -", w2ss_path, "is Not Existed !")
+            return
+
+        if 0 >= len(ss2w_path):
+            print("[KorLexAPI][INIT] ERR - Plz check ss2w_path:", ss2w_path)
+            return
+        if not os.path.exists(ss2w_path):
+            print("[KorLexAPI][INIT] ERR -", ss2w_path, "is Not Existed !")
+            return
+
+        self.w2ss_path = w2ss_path
+        self.ss2w_path = ss2w_path
+        self.is_set_w2ss_path = True
+        self.is_set_ss2w_path = True
+
+        # Check Json file path
         if 0 >= len(json_path):
             print("[KorLexAPI][INIT] ERR - Plz check jsonPath:", json_path)
             return
@@ -30,14 +55,40 @@ class KorLexAPI:
 
     ### PUBLIC ###
     def load_json_data(self):
+        print("[KorLexAPI][load_json_data] Load JSON Data, Wait...")
+        is_set_json_path = True
         if not self.is_set_json_path:
             print("[KorLexAPI][load_json_data] ERR - Plz set json path")
-            return
+            is_set_json_path = False
 
+        if not self.is_set_w2ss_path:
+            print("[KorLexAPI][load_json_data] ERR - Plz set w2ss path")
+            is_set_json_path = False
+
+        if not self.is_set_json_path:
+            print("[KorLexAPI][load_json_data] ERR - Plz set ss2w path")
+            is_set_json_path = False
+
+        if not is_set_json_path: return
+
+        ## Load w2ss, ss2w json files
+        # load w2ss json
+        print("[KorLexAPI][load_json_data] Loading w2ss json...")
+        with open(self.w2ss_path, mode="r", encoding="utf-8") as w2ss_file:
+            self.w2ss_json = json.load(w2ss_file)
+            print("[KorLexAPI][load_json_data] Loaded w2ss json !")
+
+        # load ss2w json
+        print("[KorLexAPI][load_json_data] Loading ss2w json...")
+        with open(self.ss2w_path, mode="r", encoding="utf-8") as ss2w_file:
+            self.ss2w_json = json.load(ss2w_file)
+            print("[KorLexAPI][load_json_data] Loaded ss2w json !")
+
+        # Load ontolody json
+        print("[KorLexAPI][load_json_data] Loading ontology json...")
         with open(self.json_path, mode="r", encoding="utf-8") as json_file:
             self.krx_json = json.load(json_file)
-
-        print("[KorLexAPI][load_json_data] Complete load json data.")
+            print("[KorLexAPI][load_json_data] Loaded ontology json !")
 
     def TEST(self):
         print(self.krx_json)
@@ -45,6 +96,8 @@ class KorLexAPI:
 ### TEST ###
 if "__main__" == __name__:
     json_path = "./dic/test.json"
-    krx_json_api = KorLexAPI(json_path=json_path)
+    krx_json_api = KorLexAPI(json_path=json_path,
+                             w2ss_path="",
+                             ss2w_path="")
     krx_json_api.load_json_data()
     krx_json_api.TEST()
